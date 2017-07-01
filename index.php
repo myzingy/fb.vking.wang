41242518771f37d95df2e7a443387567058c708c
wangfan <fan.wang@colorcun.com>
2017-07-01 15:05:39 +0800

    index

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="zh">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pratt - Free Bootstrap 3 Theme">
+    <meta name="author" content="Alvarez.is - BlackTie.co">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <title>Facebook Marketing Budget Rules</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <script src="./assets/jquery.min.js"></script>
+    <script src="./assets/smoothscroll.js"></script>
+    
+
+  <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style></head>
+
+  <body data-spy="scroll" data-offset="0" data-target="#navigation">
+
+    <!-- Fixed navbar -->
+	    <div id="navigation" class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#"><b>Facebook Marketing</b></a>
+	        </div>
+	        <div class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav">
+	            <li class="active"><a href="#home" class="smothscroll">Power Editor</a></li>
+	            <li class=""><a href="#desc" class="smothscroll">Ads Manager</a></li>
+	            <li class=""><a href="#showcase" class="smothScroll">Business Manager</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </div>
+
+
+	<section id="home" name="home"></section>
+	<div id="headerwrap">
+	    <div class="container">
+	    	<div class="row centered">
+	    		<div class="col-lg-12">
+					<h1><b>Adset Budget Rules</b></h1>
+					<h3>Refer to the data, set the rules, and adjust the budget automatically</h3>
+					<br>
+	    		</div>
+	    		
+	    		<div class="col-lg-2">
+	    			<h5>Setp 1:Setting rule</h5>
+	    			<p>Today / last7day / last14day, Data of various indicators,Visual logic rule</p>
+	    			<img class="hidden-xs hidden-sm hidden-md" src="./assets/arrow1.png">
+	    		</div>
+	    		<div class="col-lg-8">
+	    			<img class="img-responsive" src="./assets/app-bg.png" alt="">
+	    		</div>
+	    		<div class="col-lg-2">
+	    			<br>
+	    			<img class="hidden-xs hidden-sm hidden-md" src="./assets/arrow2.png">
+	    			<h5>Setp 2:Binding rules</h5>
+	    			<p>Campaign Binding rules,Automatic execution the rules optimization</p>
+	    		</div>
+	    	</div>
+	    </div> <!--/ .container -->
+	</div><!--/ #headerwrap -->
+
+
+	<section id="desc" name="desc"></section>
+	<!-- INTRO WRAP -->
+	<div id="intro">
+		<div class="container">
+			<div class="row centered">
+				<h1>Simple and effective</h1>
+				<br>
+				<br>
+				<div class="col-lg-4">
+					<img src="./assets/intro01.png" alt="">
+					<h3>Marketing Data</h3>
+					<p>Synchronize data per hour to ensure freshness.</p>
+				</div>
+				<div class="col-lg-4">
+					<img src="./assets/intro02.png" alt="">
+					<h3>Simple rule</h3>
+					<p>Visual assembly rules, children's toys.</p>
+				</div>
+				<div class="col-lg-4">
+					<img src="./assets/intro03.png" alt="">
+					<h3>Timed execution</h3>
+					<p>When the time comes, the rules can only wait.</p>
+				</div>
+			</div>
+	    </div> <!--/ .container -->
+	</div><!--/ #introwrap -->
+
+	<section id="showcase" name="showcase"></section>
+	<div id="showcase">
+		<div class="container">
+			<div class="row">
+				<h1 class="centered">Marketing tools GUI</h1>
+				<br>
+				<div class="col-lg-8 col-lg-offset-2">
+					<div id="carousel-example-generic" class="carousel slide">
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="3" class=""></li>
+						  <li data-target="#carousel-example-generic" data-slide-to="4" class=""></li>
+					  </ol>
+					
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner">
+					    <div class="item active">
+					      <img src="./assets/1.png" width="100%" alt="">
+					    </div>
+					    <div class="item">
+					      <img src="./assets/2.png" width="100%" alt="">
+					    </div>
+					    <div class="item">
+					      <img src="./assets/3.png" width="100%" alt="">
+					    </div>
+					    <div class="item">
+					      <img src="./assets/4.png" width="100%" alt="">
+					    </div>
+						  <div class="item">
+							  <img src="./assets/5.png" width="100%" alt="">
+						  </div>
+					  </div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<br>
+			<br>	
+		</div><!-- /container -->
+	</div>	
+
+
+	<section id="contact" name="contact"></section>
+	<div id="footerwrap">
+		<div class="container">
+			<div class="col-lg-5">
+				<h3>Facebook Marketing Tools</h3>
+				<p>
+					Efficient and simple, help the market launch
+				</p>
+			</div>
+			
+			<div class="col-lg-7">
+				<h3>contact</h3>
+				<br>
+				<!--
+				<form role="form" action="#" method="post" enctype="plain"> 
+				  <div class="form-group">
+				    <label for="name1">Your Name</label>
+				    <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+				  </div>
+				  <div class="form-group">
+				    <label for="email1">Email address</label>
+				    <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+				  </div>
+				  <div class="form-group">
+				  	<label>Your Text</label>
+				  	<textarea class="form-control" name="Message" rows="3"></textarea>
+				  </div>
+				  <br>
+				  <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+				</form>
+				-->
+				Email:<a href="mailto:wangfan021484@gmail.com">wangfan021484@gmail.com</a>
+			</div>
+		</div>
+	</div>
+	<div id="c">
+		<div class="container">
+			<p>Created by <a href="http://vking.wang/">vking.wang</a></p>
+		
+		</div>
+	</div>
+    <script src="./assets/bootstrap.js"></script>
+	<script>
+	$('.carousel').carousel({
+	  interval: 3500
+	})
+	</script>
+</body>
+</html>
