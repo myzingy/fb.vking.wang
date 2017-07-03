23546a45bf7612e108ab3a5fea8664980337fee0
wangfan <fan.wang@colorcun.com>
2017-07-03 10:03:35 +0800

    index

--- index.php
+++ index.php
@@ -3,23 +3,20 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Pratt - Free Bootstrap 3 Theme">
-    <meta name="author" content="Alvarez.is - BlackTie.co">
-    <link rel="shortcut icon" href="assets/ico/favicon.ico">
-
-    <title>Facebook Marketing Budget Rules</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="assets/css/main.css" rel="stylesheet">
-    <script src="./assets/jquery.min.js"></script>
-    <script src="./assets/smoothscroll.js"></script>
-    
-
-  <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style></head>
+        <title>Facebook Marketing Budget Rules</title>
+        <meta name="keyword" content="Marketing,Adset,Budget,Rules,Campaigns,Facebook">
+        <meta name="description" content="Facebook Marketing Budget Rules,Facebook Marketing Tools,Efficient and simple, help the market launch">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="vking.wang - fb.vking.wang">
+        <link rel="shortcut icon" href="assets/ico/favicon.ico">
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="assets/css/main.css" rel="stylesheet">
+        <script src="./assets/jquery.min.js"></script>
+        <script src="./assets/smoothscroll.js"></script>
+        <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
+    </head>
 
   <body data-spy="scroll" data-offset="0" data-target="#navigation">
 
