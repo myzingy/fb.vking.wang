6e05512e7ca0e425cde7e56bba6432a072577920
wangfan <fan.wang@colorcun.com>
2017-07-09 19:52:22 +0800

    financial_record

--- wwwroot/app/Modules/financialRecord/lib.class.php
+++ wwwroot/app/Modules/financialRecord/lib.class.php
@@ -6,17 +6,32 @@
 namespace Modules\financialRecord;
 
 class lib{
-    function __construct($id="") {
-    	$this->model=new model();
-		$id=$id?$id:I('request.id');
-		if($id){
-			$this->model->find($id);
-		}
+    const TYPE_SPEND=0;
+    const TYPE_RECHARGE=1;
+    function __construct() {
+
     }
     function orderUpdate(){
         $data=file_get_contents("php://input");
         $data=@json_decode($data,true);
         debug('orderUpdate '.date("H:i"),$data);
+        $root_id="";
+        foreach ($data['meta_data'] as $x){
+            if($x['key']=='_billing_userid'){
+                $root_id=$x['value'];
+                break;
+            }
+        }
+        if($data['status']=='on-hold' && $root_id){
+            M('financial_record')->where("order_id='{$data['order_key']}'")->delete();
+            M('financial_record')->add(array(
+                'root_id'=>$root_id,
+                'type'=>self::TYPE_RECHARGE,
+                'order_id'=>$data['order_key'],
+                'value'=>($data['total']+$data['discount_total'])*100,
+                'discount_value'=>$data['discount_total']*100,
+            ));
+        }
         var_dump('orderUpdate');
     }
 }
